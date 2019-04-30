cbb0eb5756081fb6cead3a2c6eab06e5a1ed6c00
blinkous <saradhani1@gmail.com>
2019-04-30 19:29:36 -0400

    working on home, added navbar

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+    <?php
+        title($pageTitle);
+    ?>
+    </title>
+    <link rel="stylesheet" href="pin_style.css" type="text/css"/>
+</head>
+<body>
+    <div class="container-fluid">
