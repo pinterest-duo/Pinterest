5de66f866f467785339df80b93a2e59d724593fe
blinkous <saradhani1@gmail.com>
2019-05-07 16:45:16 -0400

    can create boards

--- create_board.php
+++ create_board.php
@@ -7,13 +7,23 @@ $board_name = $_POST['board_name'];
 $is_secret_board = $_POST['is_secret_board'];
 
 echo "
+<h1>Form Info</h1>
 pin url: $url <br> 
-boardid: $board_name <br> 
+boardName: $board_name <br> 
 is_secret_board: $is_secret_board <br>
 user: $userid <br>";
 
+echo "<h1>Board Query</h1>";
+// Change the query depending on whether or not the board is secret
+if($is_secret_board == NULL){
+    echo "secret board null<br>";
+    $query = "INSERT INTO boards(board_name, cover_pin_url, secret_board, user_id) VALUES('$board_name','$url', 'YES', '$userid');";
+}
+else{
+    echo "secret board NOT null<br>";
+    $query = "INSERT INTO boards(board_name, cover_pin_url, user_id) VALUES('$board_name', '$url','$userid');";
+}
 // Create the Board
-$query = "INSERT INTO boards(board_name, cover_pin_url, user_id) VALUES('$board_name', '$url','$userid');";
 $run = mysqli_query($dbc, $query);
 if(mysqli_affected_rows($dbc) == 1){
     echo "<p>Board has been added.</p>";
@@ -22,19 +32,22 @@ else{
     echo "error".mysqli_error($dbc);
 }
 
-$query = "SELECT board_id FROM boards WHERE board_name = $board_name;";
+
+// Get the board id for the board we just created
+// $query = "SELECT board_id FROM boards WHERE board_name = '$board_name';";
+$query = "SELECT board_id FROM boards ORDER BY board_date DESC LIMIT 1;";
 $run = mysqli_query($dbc, $query);
 $row = mysqli_fetch_array($run, MYSQLI_ASSOC);
-$board = $row['board_id'];
+echo "board_id run contains: ".$row['board_id'];
+$pinBoard = $row['board_id'];
 
-echo "
-boardid: $board <br> 
-";
-
+// ERROR: WON'T CREATE THE PIN BECAUSE THE BOARD ID IS INVALID
 
 // Create the Pin
-$query = "INSERT INTO pins(pin_url, board_id, user_id) VALUES('$url','$board','$userid');";
+$query = "INSERT INTO pins(pin_url, board_id, user_id) VALUES('$url','$pinBoard','$userid');";
 $run = mysqli_query($dbc, $query);
+
+echo "<h1>Pin Query</h1>";
 if(mysqli_affected_rows($dbc) == 1){
     echo "<p>User has been edited.</p>";
 }
