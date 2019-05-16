96729e889523b3d81a06858c422677ba9b71d7c9
blinkous <saradhani1@gmail.com>
2019-05-16 11:20:43 -0400

    fixing signup

--- includes/functions.php
+++ includes/functions.php
@@ -1,23 +1,17 @@
 <script>
     $(document).ready(function(){     
-
-
     $('#signUp').modal();
-    
-
     $("#bSignUp").click(function() {
-
-                    $("#signUp").modal("show", function() {
-                        $("#logIn").modal("hide");
-                    });
+        $("#signUp").modal("show", function() {
+            $("#logIn").modal("hide");
         });
+    });
 
-        $("#blogin").click(function() {
-
-                    $("#logIn").modal("show", function() {
-                        $("#signUp").modal("hide");
-                    });
+    $("#blogin").click(function() {
+        $("#logIn").modal("show", function() {
+            $("#signUp").modal("hide");
         });
+    });
     
     }); 
 </script> 
